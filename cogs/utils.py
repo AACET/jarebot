d0bd5a8b2693e83240ebe7c59cf1309fc85db0c6
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-09-29 11:47:41 -0300

    `Cogs` - Add util commands cog

---
+++ cogs/utils.py
@@ -0,0 +1,14 @@
+from pydoc import describe
+from discord.ext import commands
+import discord
+from libs import config
+
+class Utils(commands.Cog):
+
+    @commands.command("ping", description='Uepa')
+    async def ping(self, ctx):
+        await ctx.send("🏓 Pong!")
+
+
+async def setup(bot):
+    await bot.add_cog(Utils(bot))
